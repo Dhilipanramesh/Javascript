2f039aea0da64b890df2bc85346ead3e09745595
Dhilipan-ramesh <dhilipanr.28@gmail.com>
2026-04-17 21:56:43 +0530

    adding more details about NaN and BigInt data types in JavaScript.

--- modules/Datatypes/datatype.js
+++ modules/Datatypes/datatype.js
@@ -5,10 +5,23 @@
 let age = 25;
 let price = 19.99;
 console.log(age, price);    
+let nanValue = NaN;
+console.log( typeof nanValue);
+console.log( nanValue);
 // typeof operator
 console.log(typeof age);    
 console.log(typeof price);
+// NaN is attract to any number, and any operation involving NaN will result in NaN
+let testValue = NaN + 45;
+console.log(testValue);
+
+
+
+
+
 // bigint
+// BigInt is a built-in object that provides a way to represent whole numbers larger than 2^53 - 1.
+//
 let largeNumber = 123456789012345678901234567890n;
 console.log(largeNumber);   
 
@@ -42,5 +55,4 @@ console.log(person);
 // array
 let numbers = [1, 2, 3, 4, 5];
 console.log(numbers);
-
 
